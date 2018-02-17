ce66508ff82447c49bece7552767b9ee20f2b8f4
Eli Lindahl <lindahl2010@hotmail.com>
2018-02-17 13:03:16 -0600

    Initial setup

--- Project 2/Program.cs
+++ Project 2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Project_2
 {
@@ -10,6 +11,13 @@ namespace Project_2
     {
         static void Main(string[] args)
         {
+            //Declarations
+            const char DELIM = ',';
+            const string FILE = "Super_Bowl_Project.csv";
+            FileStream file = new FileStream(FILE, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+            string record;
+            string[] info;
 
         }
     }
