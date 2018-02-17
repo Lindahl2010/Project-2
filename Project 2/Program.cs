3e28d261e765845b5acc26aa34408ac2847f3a13
Eli Lindahl <35265037+Lindahl2010@users.noreply.github.com>
2018-02-17 15:17:24 -0600

    Setup of first task to be listed in window.

--- Project 2/Program.cs
+++ Project 2/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using static System.Console;
 
 namespace Project_2
 {
@@ -16,9 +17,34 @@ namespace Project_2
             const string FILE = "Super_Bowl_Project.csv";
             FileStream file = new FileStream(FILE, FileMode.Open, FileAccess.Read);
             StreamReader reader = new StreamReader(file);
+            SBList superBowl = new SBList();
+            string headerLine = reader.ReadLine();
+            int ptDiff;
             string record;
             string[] info;
 
-        }
-    }
+            WriteLine("\n{0,-10}{1,-20}{2,-20}{3,-20}{4,-30}{5,-30}", "Year", "Team", "QB", "Coach", "MVP", "Point Difference");
+            record = reader.ReadLine();
+            while(record != null)
+            {
+                info = record.Split(DELIM);
+                superBowl.date = info[0];
+                superBowl.winner = info[5];
+                superBowl.QBWinner = info[3];
+                superBowl.coachWinner = info[4];
+                superBowl.MVP = info[11];
+                superBowl.winPts = Convert.ToInt32(info[6]);
+                superBowl.losingPts = Convert.ToInt32(info[10]);
+                ptDiff = superBowl.winPts - superBowl.losingPts;
+                ptDiff.ToString();
+                WriteLine("{0,-10}{1,-20}{2,-20}{3,-20}{4,-30}{5,-30}", superBowl.date, superBowl.winner, superBowl.QBWinner, superBowl.coachWinner, superBowl.MVP, ptDiff);
+                record = reader.ReadLine();
+            }
+
+            reader.Close();
+            file.Close();
+            ReadLine();
+
+        }//End of Main Method
+    }//End of Program Class
 }
---
+++ Project 2/SBList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2
+{
+    class SBList
+    {
+        public string date { get; set; }
+        public string SB { get; set; }
+        public int attendance { get; set; }
+        public string QBWinner { get; set; }
+        public string coachWinner { get; set; }
+        public string winner { get; set; }
+        public int winPts { get; set; }
+        public string QBLoser { get; set; }
+        public string coachLoser { get; set; }
+        public string loser { get; set; }
+        public int losingPts { get; set; }
+        public string MVP { get; set; }
+        public string stadium { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+
+    }
+}
