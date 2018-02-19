94a82c81b4728f15345fa268b25891bc3463246e
Eli Lindahl <35265037+Lindahl2010@users.noreply.github.com>
2018-02-19 16:44:45 -0600

    Modularizing program

--- Project 2/Program.cs
+++ Project 2/Program.cs
@@ -12,6 +12,16 @@ namespace Project_2
     {
         static void Main(string[] args)
         {
+            Write("\n");
+            SBWinners();
+
+            Write("\n");
+            topFive();
+
+        }//End of Main Method
+
+        public static void SBWinners()
+        {
             //Declarations
             const char DELIM = ',';
             const string FILE = "Super_Bowl_Project.csv";
@@ -23,9 +33,10 @@ namespace Project_2
             string record;
             string[] info;
 
+            WriteLine("Super Bowl Winners");
             WriteLine("\n{0,-10}{1,-20}{2,-20}{3,-20}{4,-30}{5,-30}", "Year", "Team", "QB", "Coach", "MVP", "Point Difference");
             record = reader.ReadLine();
-            while(record != null)
+            while (record != null)
             {
                 info = record.Split(DELIM);
                 superBowl.date = info[0];
@@ -45,6 +56,40 @@ namespace Project_2
             file.Close();
             ReadLine();
 
-        }//End of Main Method
+        }
+
+        public static void topFive()
+        {
+            //Declarations
+            const char DELIM = ',';
+            const string FILE = "Super_Bowl_Project.csv";
+            FileStream file = new FileStream(FILE, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+            SBList superBowl = new SBList();
+            string headerLine = reader.ReadLine();
+            string record;
+            string[] info;
+
+            WriteLine("Top Five Attended Super Bowl's");
+            WriteLine("\n{0,-10}{1,-20}{2,-20}{3,-20}{4,-30}{5,-30}", "Year", "Winning Team", "Losing Team", "City", "State", "Stadium");
+            record = reader.ReadLine();
+            while (record != null)
+            {
+                info = record.Split(DELIM);
+                superBowl.date = info[0];
+                superBowl.winner = info[5];
+                superBowl.loser = info[9];
+                superBowl.city = info[13];
+                superBowl.state = info[14];
+                superBowl.stadium = info[12];
+                WriteLine("{0,-10}{1,-20}{2,-20}{3,-20}{4,-30}{5,-30}", superBowl.date, superBowl.winner, superBowl.loser, superBowl.city, superBowl.state, superBowl.stadium);
+                record = reader.ReadLine();
+            }
+
+            reader.Close();
+            file.Close();
+            ReadLine();
+
+        }
     }//End of Program Class
 }
