44c9c0687ef1f70eee0e28122c966b897ace97c4
Eli Lindahl <lindahl2010@hotmail.com>
2018-02-22 07:26:03 -0600

    Query setup, but will move into one method that will accomplish the writing of the text file

--- Project 2/Program.cs
+++ Project 2/Program.cs
@@ -22,7 +22,7 @@ namespace Project_2
             var SBQuery =
                 from team in superbowls
                 where team.attendance > 0 
-                orderby team.SB
+                orderby team.date
                 select new { team.date, team.winner, team.QBWinner, team.coachWinner, team.MVP, team.ptDiff };
 
             
@@ -30,9 +30,61 @@ namespace Project_2
             WriteLine("\n{0,-15}{1,-30}{2,-30}{3,-20}{4,-30}{5,-30}", "Year", "Team", "QB", "Coach", "MVP", "Point Diff");
             foreach (var team in SBQuery)
             {
-                WriteLine("\n{0,-15}{1,-30}{2,-30}{3,-20}{4,-30}{5,-30}", team.date, team.winner, team.QBWinner, team.coachWinner, team.MVP, team.ptDiff);
+                WriteLine("{0,-15}{1,-30}{2,-30}{3,-20}{4,-30}{5,-30}", team.date, team.winner, team.QBWinner, team.coachWinner, team.MVP, team.ptDiff);
             }
         }
+
+        public static void topFive(List<SBList> superbowls)
+        {
+            var SBQuery =
+                from team in superbowls
+                where team.attendance > 0
+                orderby team.attendance descending
+                select new { team.date, team.winner, team.loser, team.city, team.state, team.stadium };
+
+            WriteLine("Top Five Attended Super Bowl's");
+            WriteLine("\n{0,-15}{1,-30}{2,-30}{3,-20}{4,-30}{5,-30}", "Year", "Winning Team", "Losing Team", "City", "State", "Stadium");
+            foreach (var team in SBQuery)
+            {
+                WriteLine("{0,-15}{1,-30}{2,-30}{3,-20}{4,-30}{5,-30}", team.date, team.winner, team.loser, team.city, team.state, team.stadium);
+            }
+        }
+
+        public static void host(List<SBList> superbowls)
+        {
+            //Figure out how to find which state has hosted the most Super Bowl's 
+            var SBQuery =
+                from team in superbowls
+                where team.attendance > 103700
+                orderby team.SB
+                select new { team.city, team.state, team.stadium };
+
+            WriteLine("State that hosted the Most Super Bowls");
+            WriteLine("\n{0,-15}{1,-30}{2,-30}", "City", "State", "Stadium");
+            foreach(var team in SBQuery)
+            {
+                WriteLine("\n{0,-15}{1,-30}{2,-30}", team.city, team.state, team.stadium);
+            }
+        }
+
+        public static void MVP(List<SBList> superbowls)
+        {
+            //Figure out how to find items repeated more than once 
+            var SBQuery =
+                from team in superbowls
+                where team.date != null
+                orderby team.SB
+                select new { team.MVP, team.winner, team.loser };
+
+            WriteLine("Players to receive MVP 2 or more times");
+            WriteLine("\n{0,-15}{1,-30}{2,-30}", "MVP", "Winning Team", "Losing Team");
+            foreach(var team in SBQuery)
+            {
+                WriteLine("\n{0,-15}{1,-30}{2,-30}", team.MVP, team.winner, team.loser);
+            }
+        }
+
+
 
         public static void readInData()
         {
@@ -48,10 +100,10 @@ namespace Project_2
             {
                 FileStream file = new FileStream(FILE, FileMode.Open, FileAccess.Read);
                 StreamReader reader = new StreamReader(file);
+                string headerLine = reader.ReadLine();
 
                 while (!reader.EndOfStream)
                 {
-                    string headerLine = reader.ReadLine();
                     info = reader.ReadLine().Split(DELIM);
                     listSuperBowls = new SBList(info[0], info[1], Convert.ToInt32(info[2]), info[3], info[4], info[5], Convert.ToInt32(info[6]), info[7], info[8], info[9], Convert.ToInt32(info[10]), info[11], info[12], info[13], info[14]);
                     superbowls.Add(listSuperBowls);
@@ -68,10 +120,12 @@ namespace Project_2
             
             SBWinners(superbowls);
             ReadLine();
+            topFive(superbowls);
+            ReadLine();
 
         }
 
-        public static void topFive()
+        public static void Top()
         {
             //Declarations
             const char DELIM = ',';
