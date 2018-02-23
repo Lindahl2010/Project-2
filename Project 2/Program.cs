34fa63d8270fa2f154843caac09576ad1d1cebce
Eli Lindahl <35265037+Lindahl2010@users.noreply.github.com>
2018-02-22 20:46:24 -0600

    Possibly need to restructure to make a smoother code flow if possible & keeping git updated.

--- Project 2/Program.cs
+++ Project 2/Program.cs
@@ -59,96 +59,147 @@ namespace Project_2
 
             FileStream file = new FileStream("SuperBowl.txt", FileMode.Create, FileAccess.Write);
             StreamWriter writer = new StreamWriter(file);
+            int date, x, z;
 
-            //Prints out all super bowl winners but is not correctly ordering by date
-            var SBWinners =
-                from team in superbowls
-                where team.attendance > 0 
-                orderby team.date
-                select new { team.date, team.winner, team.QBWinner, team.coachWinner, team.MVP, team.ptDiff };
-
-            writer.WriteLine("Super Bowl Winners");
-            writer.WriteLine("\n");
-            writer.WriteLine("\n{0,-15}{1,-30}{2,-30}{3,-20}{4,-30}{5,-30}", "Year", "Team", "QB", "Coach", "MVP", "Point Diff");
-            writer.WriteLine("\n");
-
-            int date;
-            int x;
-            foreach (var team in SBWinners)
+            try
             {
-                date = team.date.Length;
-                x = date - 2;
-                writer.WriteLine("\n{0,-15}{1,-30}{2,-30}{3,-20}{4,-30}{5,-30}", team.date.Remove(0,x), team.winner, team.QBWinner, team.coachWinner, team.MVP, team.ptDiff);
+
+                //Prints out all super bowl winners but is not correctly ordering by date
+                var SBWinners =
+                    from team in superbowls
+                    select new { team.date, team.winner, team.QBWinner, team.coachWinner, team.MVP, team.ptDiff };
+
+                writer.WriteLine("Super Bowl Winners");
+                writer.WriteLine("\n");
+                writer.WriteLine("\n{0,-5}{1,-30}{2,-30}{3,-20}{4,-30}{5,-30}", "Year", "Team", "QB", "Coach", "MVP", "Point Diff");
+                writer.WriteLine("\n");
+
+                foreach (var team in SBWinners)
+                {
+                    date = team.date.Length;
+                    x = date - 2;
+                    writer.WriteLine("\n{0,-5}{1,-30}{2,-30}{3,-20}{4,-30}{5,-30}", team.date.Remove(0,x), team.winner, team.QBWinner, team.coachWinner, team.MVP, team.ptDiff);
+                }
+                writer.WriteLine("\n");
+
+                //Prints the top five attended super bowls ordered by attendance
+                var topFive =
+                    from team in superbowls
+                    where team.attendance > 100000
+                    orderby team.attendance descending
+                    select new { team.date, team.winner, team.loser, team.city, team.state, team.stadium };
+
+                writer.WriteLine("Top Five Attended Super Bowl's");
+                writer.WriteLine("\n");
+                writer.WriteLine("\n{0,-3}{1,-15}{2,-30}{3,-30}{4,-20}{5,-30}{6,-30}","#", "Year", "Winning Team", "Losing Team", "City", "State", "Stadium");
+                writer.WriteLine("\n");
+
+                z = 0;
+                foreach(var team in topFive)
+                {
+                    date = team.date.Length;
+                    x = date - 2;
+                    z += 1;
+                    writer.WriteLine("\n{0,-3}{1,-15}{2,-30}{3,-30}{4,-20}{5,-30}{6,-30}", z + ")", team.date.Remove(0,x), team.winner, team.loser, team.city, team.state, team.stadium);
+                }
+                writer.WriteLine("\n");
+
+                //Figure out how to find which state has hosted the most Super Bowl's 
+                var host =
+                    from team in superbowls
+                    select new { team.city, team.state, team.stadium };
+
+                var group = superbowls.GroupBy(superbowl => superbowl.state).Where(superbowl => superbowl.Count() > 12).Select(superbowl => superbowl.Key);
+
+                writer.WriteLine("State that hosted the Most Super Bowls");
+                writer.WriteLine("\n");
+                writer.WriteLine("\n{0,-15}{1,-30}{2,-30}", "City", "State", "Stadium");
+                writer.WriteLine("\n");
+
+                foreach (var state in group)
+                {
+                    writer.WriteLine("{0,-15}", state);
+                }
+                writer.WriteLine("\n");
+
+                //Figure out how to find items repeated more than once 
+                var MVP1 =
+                    from team in superbowls
+                    where team.date != null
+                    select new { team.MVP, team.winner, team.loser };
+
+                var MVP = superbowls.GroupBy(superbowl => superbowl.MVP).Where(superbowl => superbowl.Count() > 1).Select(superbowl => superbowl.Key);
+
+                writer.WriteLine("Players to receive MVP 2 or more times");
+                writer.WriteLine("\n");
+                writer.WriteLine("\n{0,-20}{1,-30}{2,-30}", "MVP", "Winning Team", "Losing Team");
+                writer.WriteLine("\n");
+
+                foreach (var dupName in MVP)
+                {
+                    writer.WriteLine("{0,-20}", dupName);
+                }
+                writer.WriteLine("\n");
+
+                writer.WriteLine("Coach's that lost the most Super Bowl's");
+                writer.WriteLine("\n");
+                writer.WriteLine("Name");
+                writer.WriteLine("\n");
+
+                var lost = superbowls.GroupBy(superbowl => superbowl.coachLoser).Where(superbowl => superbowl.Count() == 4).Select(superbowl => superbowl.Key);
+
+                foreach (var coach in lost)
+                {
+                    writer.WriteLine(coach);
+                }
+                writer.WriteLine("\n");
+
+                writer.WriteLine("Coach to win the most Super Bowl's");
+                writer.WriteLine("\n");
+                writer.WriteLine("Name");
+                writer.WriteLine("\n");
+
+                var won = superbowls.GroupBy(superbowl => superbowl.coachWinner).Where(superbowl => superbowl.Count() > 4).Select(superbowl => superbowl.Key);
+
+                foreach(var coach in won)
+                {
+                    writer.WriteLine(coach);
+                }
+                writer.WriteLine("\n");
+
+                writer.WriteLine("Team(s) to win the most Super Bowl's");
+                writer.WriteLine("\n");
+                writer.WriteLine("Team");
+                writer.WriteLine("\n");
+
+                var winner = superbowls.GroupBy(superbowl => superbowl.winner).Where(superbowl => superbowl.Count() > 5).Select(superbowl => superbowl.Key);
+
+                foreach(var wins in winner)
+                {
+                    writer.WriteLine(wins);
+                }
+                writer.WriteLine("\n");
+
+                writer.WriteLine("Team(s) to lose the most Super Bowl's");
+                writer.WriteLine("\n");
+                writer.WriteLine("Team");
+                writer.WriteLine("\n");
+
+                var loser = superbowls.GroupBy(superbowl => superbowl.loser).Where(superbowl => superbowl.Count() > 4).Select(superbowl => superbowl.Key);
+
+                foreach(var losses in loser)
+                {
+                    writer.WriteLine(losses);
+                }
+                writer.WriteLine("\n");
+
+
+
+
             }
-            writer.WriteLine("\n");
-
-            //Prints the top five attended super bowls ordered by attendance
-            var topFive =
-                from team in superbowls
-                where team.attendance > 100000
-                orderby team.attendance descending
-                select new { team.date, team.winner, team.loser, team.city, team.state, team.stadium };
-
-            writer.WriteLine("Top Five Attended Super Bowl's");
-            writer.WriteLine("\n");
-            writer.WriteLine("\n{0,-15}{1,-30}{2,-30}{3,-20}{4,-30}{5,-30}", "Year", "Winning Team", "Losing Team", "City", "State", "Stadium");
-            writer.WriteLine("\n");
-
-            foreach (var team in topFive)
+            catch (Exception i)
             {
-                writer.WriteLine("\n{0,-15}{1,-30}{2,-30}{3,-20}{4,-30}{5,-30}", team.date, team.winner, team.loser, team.city, team.state, team.stadium);
-            }
-            writer.WriteLine("\n");
-
-            //Figure out how to find which state has hosted the most Super Bowl's 
-            var host =
-                from team in superbowls
-                where team.attendance > 0
-                orderby team.date
-                select new { team.city, team.state, team.stadium };
-
-            var group = superbowls.GroupBy(superbowl => superbowl.state).Where(superbowl => superbowl.Count() > 12).Select(superbowl => superbowl.Key);
-
-            writer.WriteLine("State that hosted the Most Super Bowls");
-            writer.WriteLine("\n");
-            writer.WriteLine("\n{0,-15}{1,-30}{2,-30}", "City", "State", "Stadium");
-            writer.WriteLine("\n");
-
-            foreach(var dupName in group)
-            {
-                writer.WriteLine("{0,-15}", dupName);
-            }
-            
-            //Figure out how to find items repeated more than once 
-            //var MVP =
-            //    from team in superbowls
-            //    where team.date != null
-            //    orderby team.date
-            //    select new { team.MVP, team.winner, team.loser };
-
-            var MVP = superbowls.GroupBy(superbowl => superbowl.MVP).Where(superbowl => superbowl.Count() > 1).Select(superbowl => superbowl.Key);
-
-            writer.WriteLine("Players to receive MVP 2 or more times");
-            writer.WriteLine("\n");
-            writer.WriteLine("\n{0,-20}{1,-30}{2,-30}", "MVP", "Winning Team", "Losing Team");
-            writer.WriteLine("\n");
-
-            foreach( var dupName in MVP)
-            {
-                writer.WriteLine(dupName);
-            }
-
-            //foreach (var team in MVP)
-            //{
-            //    writer.WriteLine("\n{0,-20}{1,-30}{2,-30}", team.MVP, team.winner, team.loser);
-            //}
-            writer.WriteLine("\n");
-
-            var lost = superbowls.GroupBy(superbowl => superbowl.coachLoser).Where(superbowl => superbowl.Count() > 1).Select(superbowl => superbowl.Key);
-
-            foreach(var coach in lost)
-            {
-                writer.WriteLine(coach);
+                WriteLine(i.Message);
             }
 
             writer.Close();
